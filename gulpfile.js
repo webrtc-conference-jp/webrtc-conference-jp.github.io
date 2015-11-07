b424b7ce06909e46d4bbea4696c80e4a4a315051
pikotea <pikotea@gmail.com>
2015-11-07 21:05:17 +0900

    jekyllをgulpから起動できるように

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,13 @@ var uglify = require("gulp-uglify");
 var browser = require("browser-sync");
 var plumber = require("gulp-plumber");
 var sourcemaps = require('gulp-sourcemaps');
+var spawn = require('child_process').spawn;
 
-//　自動更新
+// 自動更新
 gulp.task("server", function() {
     browser({
         server: {
-            baseDir: "./"
+            baseDir: "./_site/"
         }
     });
 });
@@ -22,7 +23,7 @@ gulp.task('html',function(){
     .pipe(gulp.dest("./*.html"))
     .pipe(browser.reload({stream:true}))
 });
- 
+
 // sass compile
 gulp.task("sass", function() {
     gulp.src("./scss/*.scss")
@@ -38,27 +39,28 @@ gulp.task("sass", function() {
         .pipe(browser.reload({stream:true}))
 });
 
+// jekyll compile
+gulp.task('jekyll', function () {
+    var jekyll = spawn('jekyll', ['build', '--incremental']);
+
+    jekyll.stdout.on('data', function(data) {
+        console.log("" + data);
+    });
+    jekyll.stderr.on('data', function(data) {
+        console.log("" + data);
+    });
+
+    jekyll.on('exit', function (code) {
+        var message = code ? 'error' : 'success'
+        console.log('Finished Jekyll Build : ' + message);
+        browser.reload();
+    });
+});
+
 // gulp監視
 gulp.task("default",['server'], function() {
-    gulp.watch("./scss/*.scss",["sass"]);
     gulp.watch(['*.html'],['html']);    //htmlファイルを監視
     gulp.watch(['./scss/*.scss'],['sass']); //scssファイルを監視
     gulp.watch(['./css/*.css'],['html']); //cssファイルを監視
+    gulp.watch(['./index.html', './session/*.html', './sponsor/*.html', './_data/*.json'],["jekyll"]);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
